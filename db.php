4c0ac29e7aac17f0b31e4e99c5c0461f8712fa27
Niroshan26 <niroshanaks80@gmail.com>
2026-06-05 19:21:32 +0530

    Update database host and password credentials

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
-$host = "127.0.0.1:3307";
+$host = "127.0.0.1";
 $db_user = "root";       
-$db_pass = "root";           
+$db_pass = "1234";           
 $db_name = "smart_tuition"; 
 
 // Database එකට සම්බන්ධ වීම
